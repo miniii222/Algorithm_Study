71a314c4dca5674939e3d6c66334a6ed9630368a
Min <42695789+miniii222@users.noreply.github.com>
2018-12-23 18:15:55 +0900

    Add files via upload

--- SDS_test/L2_3_dinner.py
+++ SDS_test/L2_3_dinner.py
@@ -16,38 +16,39 @@ for i in range(N) :
     xy[i] = input().split(' ')
     xy[i] = list(map(int, xy[i]))
     
-
 #%%function
 def dinner(xy) :
     
     xy2 = xy.copy()
     xy2.sort()
-
-    xy = [(i+1,a) for i,a in enumerate(xy)]
+    
+    xy = [(i,a) for i,a in enumerate(xy)]
     xy.sort(key = lambda x : x[1])
     
+    xy_idx = [i[0] for i in xy]
+    
     fail = []
+
     for i in range(1,N) :
         for xyxy in xy2[:i] :
             if xyxy[1]<=xy2[i][1] :
-                fail.append(xy2[i]) #후보에서 탈락한 레스토랑 목록
+                fail+= [ xy_idx[i] ]
                 break
-    return [i for i, rest in xy if rest not in fail]
+        
+    return [idx+1 for idx in xy_idx if idx not in fail]
 
 #%%output
 T = int(input())
 
-for tt in range(T) :
+for tt in range(T+1) :
     N = int(input()) #점의 개수
 
     xy = [None]*N
     for i in range(N) :
         xy[i] = input().split(' ')
         xy[i] = list(map(int, xy[i]))
-        
-    print("#{} ".format(tt+1), end = "")
-    dd = dinner(xy)
-    for answer in dd :
-        print(answer, end = " ")
-    print("")
+    
+    answer = " ".join(str(x) for x in dinner(xy))
+    print("#{} {}".format(tt+1, answer))
+
           
