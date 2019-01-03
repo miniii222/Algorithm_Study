5936b041f3bf7b6c354e5b50a93060ad594a4e1c
Min <42695789+miniii222@users.noreply.github.com>
2019-01-03 12:58:51 +0900

    Add files via upload
    various examples by recursion algorithm

--- [KOOC] 데이터 구조 및 분석/week4_1.recursion_example.py
+++ [KOOC] 데이터 구조 및 분석/week4_1.recursion_example.py
@@ -12,18 +12,24 @@ for itr in range(10) :
     print(Fibonacci(itr))
     
 #%%GCD A와 B의 최대공약수를 Euclid's algorithm을 이용하여 구한다.
-def GCD(A,B) :
-        
+def GCD(numbers) :
+    
+    A,B = numbers
+    
     if B==0 :
         return A
     
     A,B = B, A%B
-    return(A, B)
+    numbers = A,B
+    return GCD(numbers)
 
+numbers = (105,74)
+print(GCD(numbers))
 #%%factorial
+def Factorial(n) :
+    if n==1 or n==0 :
+        return 1
+    
+    return Factorial(n-1) * n
 
-
-a = 32; b = 24
-a,b = b, a%b
-
-print(GCD(32,24))
+Factorial(5)
